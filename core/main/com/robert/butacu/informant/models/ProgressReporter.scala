d451f41e3064dd1a7b04093276f0d25e15e1683d
robertbutacu <robertbutacu@gmail.com>
2020-11-22 21:30:04 +0000

    More adjustments - better models

--- core/main/com/robert/butacu/informant/models/ProgressReporter.scala
+++ core/main/com/robert/butacu/informant/models/ProgressReporter.scala
@@ -8,22 +8,28 @@ import com.robert.butacu.informant.models.atoms._
 import scala.language.higherKinds
 
 trait Estimator[A] {
-  def estimate(current: A, total: A): Instant
+  def estimate(current: A, total: A):      Instant
+  def getPercentage(current: A, total: A): Percentage
 }
 
-case class Progress[A: Estimator](name:               ProcessDetails,
-                                  total:              Totality[A],
-                                  progressed:         ProgressReached[A],
-                                  reportedAt:         Instant,
-                                  expectedToFinishAt: Instant)
+case class ProgressStatus[A: Estimator](progress: ProgressReached[A], total: Totality[A])
 
-case class InterruptedReport[A: Estimator](progress: Progress[A], timeSpent: TimeSpent, reason: FailureReason[A])
+case class Progress[A: Estimator](
+                                   name:        ProcessDetails,
+                                   total:       Totality[A],
+                                   progressed:  ProgressStatus[A],
+                                   timeDetails: TimeDetails
+                                 )
+
+case class InterruptedReport[A: Estimator](progress: Progress[A], reason: FailureReason[A]) {
+  def timeSpent: TimeSpent = ??? // get from progress -> time details
+}
 case class Report[A: Estimator](progress: Progress[A], timeSpent: TimeSpent)
 
 trait ProgressReporter[F[_]] {
   implicit val clock: Clock[F]
-  def reportInterrupt[A: Estimator](progress: Progress[A]):             F[InterruptedReport[A]]
-  def reportCompleted[A: Estimator](progress: Progress[A]):             F[Report[A]]
-  def updateProgress[A: Estimator](current: Progress[A]):               F[Progress[A]]
-  def start[A: Estimator](process: ProcessDetails, total: Totality[A]): F[Progress[A]]
+  def reportInterrupt[A: Estimator](progress: Progress[A]):                     F[InterruptedReport[A]]
+  def reportCompleted[A: Estimator](progress: Progress[A]):                     F[Report[A]]
+  def update[A: Estimator](current: Progress[A], progress: ProgressReached[A]): F[Progress[A]]
+  def start[A: Estimator](process: ProcessDetails, total: Totality[A]):         F[Progress[A]]
 }
--- core/main/com/robert/butacu/informant/models/atoms/package.scala
+++ core/main/com/robert/butacu/informant/models/atoms/package.scala
@@ -2,10 +2,16 @@ package com.robert.butacu.informant.models
 
 import java.time.Instant
 
+import scala.concurrent.duration.FiniteDuration
+
 package object atoms {
   case class ProcessDetails(name: String, details: Map[String, String])
   case class Totality[A](value: A)
   case class ProgressReached[A](value: A)
   case class TimeSpent(value: Instant)
   case class FailureReason[A](message: A, error: Option[Throwable])
+  case class Percentage(value: Double)
+  case class TimeDetails(startedAt: Instant, estimatedCompletion: Instant, reportedAt: Instant) {
+    def getTimeSpent: FiniteDuration = ???
+  }
 }
